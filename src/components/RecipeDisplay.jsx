3871d53cd028514c6115f2c5bc8e5b8f43e08176
Redcheeks <lillyoleole@gmail.com>
2023-10-10 12:55:26 +0200

    fixes for display

--- src/components/RecipeDisplay.jsx
+++ src/components/RecipeDisplay.jsx
@@ -1,8 +1,9 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
+import { Recipe } from '../classes/Recipe';
 
 export default function RecipeDisplay({Recipe}) {
-const [unit, setUnit] = useState(false); // boolean state based on switch for which unit shown.
+const [unit, setUnit] = useState(Recipe.unit); // boolean state based on switch for which unit shown.
 const ingredient_list = Object.keys(Recipe.ingredients);
 const [dispIng, setDispIng] = useState(Recipe.ingredients); // This can change when units are changed?
     return (
@@ -16,14 +17,15 @@ const [dispIng, setDispIng] = useState(Recipe.ingredients); // This can change w
                 </Col>
                 <Col>
                 <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={e => unitChange(e, unit, setUnit)} checked= {unit}/>
+                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" 
+                onChange={e => unitChange(e, unit, setUnit, dispIng, setDispIng)} checked= {unit}/>
                 <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Switch to US units</label>
             </div>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <small>{Recipe.time} Minutes</small>
+                    <small>By: <i>{Recipe.author}</i> Approx time: {Recipe.time} Minutes</small>
                     <p>{Recipe.description}</p>
                     <hr/>
                 </Col>
@@ -35,7 +37,7 @@ const [dispIng, setDispIng] = useState(Recipe.ingredients); // This can change w
                         
                         {ingredient_list.map((ingredient) => 
                         <li key={ingredient}>
-                            {ingredient} - {(Object.entries(Recipe.ingredients[ingredient])[0]).join(" ") }</li>)}
+                            {ingredient} - {(Object.entries(dispIng[ingredient])[0]).join(" ") }</li>)}
                     </ul>
                 </Col>
                 <Col>
@@ -50,8 +52,18 @@ const [dispIng, setDispIng] = useState(Recipe.ingredients); // This can change w
     )
 }
 
-function unitChange(e, unit, setUnit) {
+function unitChange(e, unit, setUnit, dispIng, setDispIng) {
 
 setUnit(!unit);
+/*TODO: implement unit conversion with api. */
+/*const converted_values = Object.keys(dispIng).map((name) => {
+    return {name : convertIngredient(dispIng[name], unit)}
+});
 
+setDispIng(converted_values)*/
+}
+
+function convertIngredient(item, unit){
+
+    return item
 }
