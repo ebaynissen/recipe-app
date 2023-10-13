d2e7dad7fa39598ba03164b8471131a77b0ede38
Ebbe Arildsson <ebbe.arildsson@gmail.com>
2023-10-13 17:33:18 +0200

    🎨 Cleanup!

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,22 +1,16 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 
 export default function RecipeList({recipes, setView}) {
-    
-
     return (
     <Container>
         <h4> Recipes to choose from: </h4>
 
         {recipes.map((rec) => 
-        <li key={rec.id}>
-            <button key={"Button"+rec} onClick={() => setView(rec)}>
+        <li className='p-1' key={rec.id}>
+            <button type='button' class="btn btn-info" key={"Button"+rec} onClick={() => setView(rec)}>
                 <b>{rec.name}</b> <i>by {rec.author}</i>
             </button>
         </li>)}
-
-
-           
     </Container>
     )
 }
