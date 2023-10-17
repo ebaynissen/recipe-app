80a780af6d3481cf0c508a54026b6316b611210e
Lukas Kraft <ebaynissen@gmail.com>
2023-10-17 21:26:33 +0200

    👽 Implemented openFood API

--- src/api/OpenFoodFacts.jsx
+++ src/api/OpenFoodFacts.jsx
@@ -6,30 +6,34 @@ import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default function OpenFoodFacts() {
-    const [foodItem, setFoodItem] = useState("Orange");
+    const [foodItem, setFoodItem] = useState("");
     const [foodData, setFoodData] = useState("");
 
     const fetchFoodFacts = async (food) => {
-        setFoodItem(food);
         const foodData = await fetch(`https://world.openfoodfacts.org/api/v2/search?categories_tags_en=${food}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data.products[0].nutriscore_data);
-                setFoodData(data.products[0].nutriscore_data.energy);
+                console.log(data.products);
+                setFoodItem(data.products.filter(product => product.countries_lc === "en")[0].product_name);
+                setFoodData(data.products.filter(product => product.countries_lc === "en")[0].nutriscore_data.grade.toUpperCase());
                 return data;
             }
                 
             ).catch(error => {
                 console.log(error);
             });
+        return foodData;
     }
 
     return (
         <Container>
             <Row>
                 <Col>
-                    <h1>Open Food Facts</h1>
-                    <p>Search for a food item to get its description</p>
+                    <h2>Open Food Facts</h2>
+                    <hr />
+                    <p>Search for a food item of your choice and the API will fetch the first item
+                        that matches the name and return its nutritional grade.
+                    </p>
                 </Col>
             </Row>
             <Row>
@@ -39,8 +43,19 @@ export default function OpenFoodFacts() {
                 </Col>
             </Row>
             <Row>
-                <Col className="m-4">
-                    {foodData}
+                <Col>
+                    <h4 className="my-2">Nutrition Grade: </h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {foodItem ?? "No data"}
+                </Col>
+                <Col>
+                    
+                </Col>
+                <Col>
+                    <h4 className='text-danger'>{foodData ?? "F"}</h4>
                 </Col>
             </Row>
         </Container>
