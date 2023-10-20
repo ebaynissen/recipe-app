61c368b41910effe1eab6d88c4fe361058bd2364
Lukas Kraft <69203755+ebaynissen@users.noreply.github.com>
2023-10-20 15:41:06 +0200

    Update RecipePreview.jsx

--- src/components/RecipePreview.jsx
+++ src/components/RecipePreview.jsx
@@ -1,34 +1,49 @@
-import { Row, Col, Form, Card, Button, FormLabel} from 'react-bootstrap';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { Row, Col, Form, Card, Button, FormLabel } from "react-bootstrap";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export default function RecipePreview({Recipe, setSelectedRecipe}) {
-
+export default function RecipePreview({ Recipe, setSelectedRecipe }) {
     return (
-    <Card border="dark" className='' onClick={() => {setSelectedRecipe(Recipe)}} style={{ height: '18rem', cursor: "pointer"}}>
-        <Card.Body>
-        <Row>
-            <Col>
-                <Card.Img style={{ height: '16rem' , "object-fit": "cover" }} variant="top" 
-                src={Recipe.image}
-                alt="not found"
-                width={"80px"}/>
-            </Col>
-        
-            <Col>
+        <Card
+            border="dark"
+            className=""
+            onClick={() => {
+                setSelectedRecipe(Recipe);
+            }}
+            style={{ height: "18rem", cursor: "pointer" }}
+        >
+            <Card.Body>
                 <Row>
-                    <Card.Title><h1>{Recipe.name}</h1></Card.Title>
+                    <Col>
+                        <Card.Img
+                            style={{ height: "10rem", "object-fit": "cover" }}
+                            variant="top"
+                            src={Recipe.image}
+                            alt="not found"
+                            width={"80px"}
+                        />
+                    </Col>
+
+                    <Col>
+                        <Row>
+                            <Card.Title>
+                                <h1>{Recipe.name}</h1>
+                            </Card.Title>
+                        </Row>
+                        <Row>
+                            <small>
+                                By: <i>{Recipe.author}</i>{" "}
+                                <div className="vr mx-2"></div>Time:{" "}
+                                {Recipe.time} Minutes{" "}
+                                <div className="vr mx-2"></div>{" "}
+                            </small>
+                            <small>Portions: {Recipe.portions}</small>
+                            <hr />
+                            <p>{Recipe.description}</p>
+                        </Row>
+                    </Col>
                 </Row>
-                <Row>
-                    <small>By: <i>{Recipe.author}</i> <div className="vr mx-2"></div>Time: {Recipe.time} Minutes <div className="vr mx-2"></div> </small>
-                    <small>Portions: {Recipe.portions}</small>
-                    <hr/>
-                    <p>{Recipe.description}</p>
-                 </Row>
-            </Col>
-        </Row>
-    
-        </Card.Body>
-    </Card>
-    )
+            </Card.Body>
+        </Card>
+    );
 }
